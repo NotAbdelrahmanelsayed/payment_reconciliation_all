0ea62c449d6bcc75151790b5b2cb728db1ccf6b2
notabdelrahmanelsayed <bedoelsayed785@gmail.com>
2026-02-27 23:59:16 +0200

    fix: exception error

--- payment_reconciliation_all/reconcile.py
+++ payment_reconciliation_all/reconcile.py
@@ -146,10 +146,10 @@ def reconcile_customer_batch(
                     n_invoices,
                 )
                 success = False
-                try:
-                    e = e
-                except Exception:
-                    e = ""
+            try:
+                e = e
+            except Exception:
+                e = ""
             log = log_customer(customer, success, n_invoices, n_payments, e)
             logger.debug(
                 "Successfully Inserted |log: %s | for customer %s", log, customer
