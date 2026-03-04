d8cf31177db69a5b1ffb529438c2dcc73cb4312a
notabdelrahmanelsayed <bedoelsayed785@gmail.com>
2026-03-04 21:08:59 +0200

    style: make buttons pretty

--- payment_reconciliation_all/public/js/payment_reconciliation.js
+++ payment_reconciliation_all/public/js/payment_reconciliation.js
@@ -1,26 +1,24 @@
 frappe.ui.form.on("Payment Reconciliation", {
   refresh: function (frm) {
     frm.add_custom_button(__("Reconcile All"), function () {
-      // call server method
-      //   if (!frm.company) {
-      //     frappe.throw({
-      //       message: "Please set the Company",
-      //     });
-      //   }
+      if (!frm.doc.company) {
+        frappe.throw({ message: __("Please set the Company") });
+      }
       frappe.call({
         method: "payment_reconciliation_all.reconcile.start_reconciliation",
         args: {
-          company: frm.company,
+          company: frm.doc.company,
         },
         callback: function (r) {
           if (r.message) {
-            frappe.message(r.message);
+            frappe.msgprint(r.message);
           }
         },
       });
-    });
-    frm.add_custom_button("Bulk Reconciliation Log", () => {
+    }, __("Bulk Process"));
+
+    frm.add_custom_button(__("Bulk Reconciliation Log"), () => {
       frappe.set_route("List", "Bulk Payment Reconciliation Log");
-    });
+    }, __("Bulk Process"));
   },
 });
